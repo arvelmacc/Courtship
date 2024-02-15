2ca05c48c3dca730619a5fc38bc56522b14d2289
arvelmacc <95877424+arvelmacc@users.noreply.github.com>
2024-02-15 17:32:11 +0800

    script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "I LOVE YOU!!!";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -44,7 +44,7 @@ function generateMessage(noCount) {
   const messages = [
     "No",
     "Are you sure?",
-    "Pookie please",
+    "Lovie! please!!!",
     "Don't do this to me :(",
     "You're breaking my heart",
     "I'm gonna cry...",
